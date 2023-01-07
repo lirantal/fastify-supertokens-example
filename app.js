f7bf0776da9ab82c08e319c25fe3ddb34e6f5b91
Liran Tal <liran.tal@gmail.com>
2023-01-07 21:03:01 +0200

    refactor: plugin does the export

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 import Fastify from "fastify";
-import fastifyPlugin from "fastify-plugin";
 
 import indexRoutes from "./routes/index.js";
 
 export default async function appFramework() {
   const fastify = Fastify({ logger: true });
-  fastify.register(fastifyPlugin(indexRoutes));
+  fastify.register(indexRoutes);
 
   await fastify.ready();
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
-export default async function indexRoutes(server, options) {
+import fastifyPlugin from "fastify-plugin";
+
+async function indexRoutes(server, options) {
   server.get("/", async (request, reply) => {
     return {
       hello: "hello world",
     };
   });
 }
+
+export default fastifyPlugin(indexRoutes);
